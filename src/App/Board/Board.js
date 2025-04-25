9240c2b3ad70a4884bb7a82c78e5e1077e6f0c95
Nick Beaudoin <Nick.Beaudoin@cambiumlearning.com>
2025-04-25 15:57:07 -0400

    Add buttons

--- src/App/Board/Board.js
+++ src/App/Board/Board.js
@@ -1,9 +1,15 @@
 import './Board.css';
+import Button from './Button/Button'
+
+const spaces = [1,2,3,4,5,6,7,8,9]
+const buttons = spaces.map(space => 
+    <Button spaceNumber={space} />
+);
 
 function Board() {
   return (
     <div className="Board">
-      
+        {buttons}
     </div>
   );
 }
---
+++ src/App/Board/Button/Button.js
@@ -0,0 +1,12 @@
+import './Button.css';
+
+
+function Button({ spaceNumber }) {
+  return (
+    <div className="Button">
+        {spaceNumber}
+    </div>
+  );
+}
+
+export default Button;
